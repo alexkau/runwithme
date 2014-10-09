8b0cf59491d25e33e957c2eda55eadd965444837
Alex Kau <pr0grammer111@gmail.com>
2014-10-09 14:07:15 -0400

    Final fixes/changes

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -23,12 +23,29 @@ function getAveragePace(user) {
   for (var i=0; i < runs.length; i++) {
     paces.push(runs[i]["time"] / runs[i]["distance"]);
   }
+  // var paces = user.get("paces");
+  // if (!paces) {
+  //   return -1;
+  // }
 
   var sum = 0;
   for (var i=0; i < paces.length; i++) {
     sum += paces[i];
   }
   return sum / paces.length;
+}
+
+function getAverageDistance(user) {
+  var runs = user.get("runs");
+  if (!runs) {
+    return -1;
+  }
+
+  var sum = 0;
+  for (var i=0; i < runs.length; i++) {
+    sum += runs[i]["distance"];
+  }
+  return sum / runs.length;
 }
 
 Parse.Cloud.define("usersNearMe", function(request, response) {
@@ -42,37 +59,46 @@ Parse.Cloud.define("usersNearMe", function(request, response) {
       requestor = result[0];
 
       res = []
-      starred = new Parse.Query("User");
-      starred = new Parse.Query("User");
-      starred.notEqualTo("username", requestor.get("username"));
-      starred.containedIn("username", requestor.get("starred"));
-      // might filter too much?
-      starred.near("location", requestor.get("location"));
-      starred.find({
-        success: function(results) {
-          for (var i=0; i < results.length; i++) {
-            res.push({"fbusername": results[i].get("fbusername"), "distance": results[i].get("location").milesTo(requestor.get("location")), "starred": 1});
-          }
-        },
-        error: function() {
+      // starred = new Parse.Query("User");
+      // starred.notEqualTo("username", requestor.get("username"));
+      // starred.containedIn("username", requestor.get("starred"));
+      // // might filter too much?
+      // starred.near("location", requestor.get("location"));
+      // starred.find({
+      //   success: function(results) {
+      //     for (var i=0; i < results.length; i++) {
+      //       res.push({"fbusername": results[i].get("fbusername"), "distance": results[i].get("location").milesTo(requestor.get("location")), "starred": 1});
+      //     }
+      //   },
+      //   error: function() {
 
-        }
-      });
+      //   }
+      // });
 
       q2 = new Parse.Query("User");
       // All users except requesting user
       q2.notEqualTo("username", request.params.username);
       // Order by distance to requestor location
-      starred.notContainedIn("username", requestor.get("starred"));
-      q2.withinMiles("location", requestor.get("location"), 5);
+      // starred.notContainedIn("username", requestor.get("starred"));
+      q2.withinMiles("location", requestor.get("location"), 5000);
       q2.find({
         success: function(results) {
-          var requestorAveragePace = getAveragePace(requestor);
+          //var requestorAveragePace = getAveragePace(requestor);
+          var requestorAveragePace = requestor.get("avgPace")
           for (var i=0; i < results.length; i++) {
             // Add to list if they're within 25% of our pace
-            var averagePace = getAveragePace(results[i]);
-            if (averagePace == -1 || requestorAveragePace == -1 || (averagePace <= requestorAveragePace * 1.25 && averagePace >= requestorAveragePace * 0.75)) {
-              res.push({"fbusername": results[i].get("fbusername"), "distance": results[i].get("location").milesTo(requestor.get("location")), "starred": 0});
+            // var averagePace = getAveragePace(results[i]);
+            // var averageDistance = getAverageDistance(results[i]);
+            var averagePace = results[i].get("avgPace");
+            var averageDistance = results[i].get("avgDistance");
+            console.log(averagePace);
+            if (1==1 || !averagePace || !requestorAveragePace  || (averagePace <= requestorAveragePace * 1.33 && averagePace >= requestorAveragePace * 0.75)) {
+              res.push({
+                  "fbusername": results[i].get("fbusername")
+                , "distance": results[i].get("location").milesTo(requestor.get("location"))
+                , "averagepace": averagePace
+                , "averagedistance": averageDistance
+                , "realName": results[i].get("realName")});
             } else {
               //res.push("skipped: " + averagePace + " vs " + requestorAveragePace);
             }
